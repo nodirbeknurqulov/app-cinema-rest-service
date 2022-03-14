b5c58dd523f1ee5eaae353892445799885b4b77d
nodirbeknurqulov <nodirbeknurqulov096@gmail.com>
2022-03-14 16:18:58 +0500

    initial commit

---
+++ src/main/java/uz/pdp/appcinemarestservice/AppCinemaRestServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appcinemarestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCinemaRestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCinemaRestServiceApplication.class, args);
+    }
+
+}
